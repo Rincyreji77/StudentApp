d648671d88a5c045de90d122d77c5851f9df38f6
rincyreji <rincy667@gmail.com>
2022-05-17 11:41:59 +0530

    message 7

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AddStudent from './component/AddStudent';
 import Searchstudent from './component/Searchstudent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
    <div>
-     <AddStudent/>
-     <Searchstudent/>
+ <BrowserRouter>
+  <Routes>
+    <Route path="/" exact element={<AddStudent/>}/>
+    <Route path="/search" exact element={<Searchstudent/>}/>
+
+  </Routes>
+  </BrowserRouter>
    </div>
   );
 }
