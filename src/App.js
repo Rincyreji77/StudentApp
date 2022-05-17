1f32ac080c9f381e28e22395120afb5a7966a64f
rincyreji <rincy667@gmail.com>
2022-05-17 11:23:45 +0530

    message 5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AddStudent from './component/AddStudent';
+import Searchstudent from './component/Searchstudent';
 
 function App() {
   return (
    <div>
      <AddStudent/>
+     <Searchstudent/>
    </div>
   );
 }
